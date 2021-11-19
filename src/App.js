ff14167c22b8d593e1d5c568056a5f840561ddc3
jeremy-aubrey <jeremyaubrey@gmail.com>
2021-11-19 15:27:13 -0600

    added Routes to pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+
+import AllMeetUpsPage from "./pages/AllMeetUps";
+import NewMeetUpsPage from "./pages/NewMeetUp";
+import FavoritesPage from "./pages/Favorites";
+
 function App() {
-  return <div>Hello!</div>;
+  /*
+  React Router 6
+  - path prop used to specify a url 
+  - element prop used to specify the page component to be loaded
+  */
+  return (
+    <Routes>
+      <Route path="/" element={<AllMeetUpsPage />}></Route>
+      <Route path="/new-meetup" element={<NewMeetUpsPage />}></Route>
+      <Route path="/favorites" element={<FavoritesPage />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
