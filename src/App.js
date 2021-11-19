ece34e0d1a055752c008217347326ffae4b9f84b
jeremy-aubrey <jeremyaubrey@gmail.com>
2021-11-19 15:40:48 -0600

    imported MainNavigation, nested routes into main

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetUpsPage from "./pages/AllMeetUps";
 import NewMeetUpsPage from "./pages/NewMeetUp";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   /*
@@ -11,11 +12,16 @@ function App() {
   - element prop used to specify the page component to be loaded
   */
   return (
-    <Routes>
-      <Route path="/" element={<AllMeetUpsPage />}></Route>
-      <Route path="/new-meetup" element={<NewMeetUpsPage />}></Route>
-      <Route path="/favorites" element={<FavoritesPage />}></Route>
-    </Routes>
+    <div>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<AllMeetUpsPage />}></Route>
+          <Route path="/new-meetup" element={<NewMeetUpsPage />}></Route>
+          <Route path="/favorites" element={<FavoritesPage />}></Route>
+        </Routes>
+      </main>
+    </div>
   );
 }
 
