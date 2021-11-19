ae719c16271080b67bcb58f5dfa3a8369b7a09f0
jeremy-aubrey <jeremyaubrey@gmail.com>
2021-11-19 16:52:46 -0600

    added MeetUpItem and MeetUpList components

---
+++ src/components/meetups/MeetUpItem.js
@@ -0,0 +1,21 @@
+import classes from "./MeetUpItem.module.css";
+
+const MeetUpItem = (props) => {
+  return (
+    <li className={classes.item}>
+      <div className={classes.image}>
+        <img src={props.image} alt=""></img>
+      </div>
+      <div className={classes.content}>
+        <h3>{props.title}</h3>
+        <address>{props.address}</address>
+        <p>{props.description}</p>
+      </div>
+      <div className={classes.actions}>
+        <button>Add to Favorites</button>
+      </div>
+    </li>
+  );
+};
+
+export default MeetUpItem;
