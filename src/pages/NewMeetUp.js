6c61b7ce8dd53649f841e2696c1c68b624e6dc1a
jeremy-aubrey <jeremyaubrey@gmail.com>
2021-11-20 11:30:21 -0600

    uses fetch to send POST request to firebase

--- src/pages/NewMeetUp.js
+++ src/pages/NewMeetUp.js
@@ -2,13 +2,26 @@ import NewMeetUpForm from "../components/meetups/NewMeetUpForm";
 
 const NewMeetUpsPage = () => {
   const addMeetUpHander = (meetUpData) => {
-    console.log(meetUpData);
-  }
-  
+    //firebase db url,
+    //config object for post request - data should be in JSON format
+    //JSON.stringify() to convert meetUpData to JSON format
+
+    fetch(
+      "https://react-demo-project-1-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetUpData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  };
+
   return (
     <section>
       <h1>Add New meetup</h1>
-      <NewMeetUpForm onAddMeetUp={addMeetUpHander}/>
+      <NewMeetUpForm onAddMeetUp={addMeetUpHander} />
     </section>
   );
 };
