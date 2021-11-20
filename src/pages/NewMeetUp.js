cd91a21934d5eede827a6f7c9cd13853c10fef72
jeremy-aubrey <jeremyaubrey@gmail.com>
2021-11-20 11:20:47 -0600

    added handler to recieve form data on submit

--- src/pages/NewMeetUp.js
+++ src/pages/NewMeetUp.js
@@ -1,10 +1,14 @@
 import NewMeetUpForm from "../components/meetups/NewMeetUpForm";
 
 const NewMeetUpsPage = () => {
+  const addMeetUpHander = (meetUpData) => {
+    console.log(meetUpData);
+  }
+  
   return (
     <section>
       <h1>Add New meetup</h1>
-      <NewMeetUpForm />
+      <NewMeetUpForm onAddMeetUp={addMeetUpHander}/>
     </section>
   );
 };
