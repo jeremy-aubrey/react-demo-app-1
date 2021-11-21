01084dc10604f2f51bdc7f8647e9a0a22afef2e8
jeremy-aubrey <jeremyaubrey@gmail.com>
2021-11-21 13:42:08 -0600

    accesses global favorites context, populates items via context

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,30 @@
+import { useContext } from "react";
+
+import FavoritesContext from "../store/favorites-context";
+import MeetUpList from "../components/meetups/MeetUpList";
+
 const FavoritesPage = () => {
-  return <div>Favorites Page</div>;
+  /*
+  Note: Since the favorites context exists only in memory
+  the content will be lost on refresh. For peristance, 
+  adding a favorite would need POST to a server which we could 
+  make a GET request to here. (Like in all meetups)
+  */
+  const favoriteCtx = useContext(FavoritesContext);
+
+  let content;
+  if (favoriteCtx.totalFavorites === 0) {
+    content = <p>You have no favorites yet. Why don't you add some?</p>;
+  } else {
+    content = <MeetUpList meetups={favoriteCtx.favorites} />;
+  }
+
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
 };
 
 export default FavoritesPage;
