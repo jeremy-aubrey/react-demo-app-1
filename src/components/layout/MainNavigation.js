2fadca09bdb8e33e40a55f9c6f400043d63fe1dc
jeremy-aubrey <jeremyaubrey@gmail.com>
2021-11-21 13:40:56 -0600

    accesses global favorites context, added badge to display favorites total

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
+import FavoritesContext from "../../store/favorites-context";
 
 const MainNavigation = () => {
+  const favoritesCtx = useContext(FavoritesContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>Meetups</div>
@@ -15,7 +19,12 @@ const MainNavigation = () => {
             <NavLink to="/new-meetup">New Meetup</NavLink>
           </li>
           <li>
-            <NavLink to="/favorites">My Favorites</NavLink>
+            <NavLink to="/favorites">
+              My Favorites
+              <span className={classes.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>
+            </NavLink>
           </li>
         </ul>
       </nav>
