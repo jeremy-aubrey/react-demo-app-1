d19d8cbb14cf96b000bbb5f1cece480cd4e76799
jeremy-aubrey <jeremyaubrey@gmail.com>
2021-11-21 11:45:30 -0600

    added addFavoriteHandler to context

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -1,23 +1,32 @@
-import { createContext, useState }  from 'react';
+import { createContext, useState } from "react";
 
-const FavoritesContext = createContext({ 
-    favorites: [],
-    totalFavorites: 0
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
 });
 
 const FavoritesContextProvider = (props) => {
-    const [userFavorites, setUserFavorites] = useState([]);
+  const [userFavorites, setUserFavorites] = useState([]);
 
-    const addFavoriteHandler = () => {}
-    const removeFavoriteHander = () => {}
-    const itemIsFavtorite = () => {}
-    
-    const context = {
-        favorites: userFavorites,
-        totalFavorites: userFavorites.length,
-    };
+  const addFavoriteHandler = (favoriteMeetUp) => {
+    setUserFavorites((prevState) => {
+      return prevState.concat(favoriteMeetUp);
+    });
+  };
 
-    return <FavoritesContext.Provider value={context}>
-        {props.children}
+  const removeFavoriteHander = () => {};
+
+  const itemIsFavtorite = () => {};
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+    addFavorite: addFavoriteHandler,
+  };
+
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
     </FavoritesContext.Provider>
-}
+  );
+};
